cfd0de685697c57b2b11989a347ef5dafe6c4e3e
Anshu kumari <109532001+kumarianshu12@users.noreply.github.com>
2022-07-20 06:35:35 -0700

    Add files via upload

---
+++ A.java
@@ -0,0 +1,8 @@
+class A
+{
+public static void main (String args[])
+{
+System.out.println("a");
+System.out.println("\n a");
+}
+} 
---
+++ AB.java
@@ -0,0 +1,16 @@
+class AB
+{
+public static void main(String args[])
+{
+int a=10;
+int b=20;
+int c=a+b;
+int d=a-b;
+int e=a*b;
+int f=a/b;
+System.out.println("sum of"+(a+b));
+System.out.println("sub of"+(a-b));
+System.out.println("multi of"+(a*b));
+System.out.println("div of"+(a/b));
+}
+}
