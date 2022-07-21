08ea0360c830330824762baf98ca471dc21c497c
Anshu kumari <109532001+kumarianshu12@users.noreply.github.com>
2022-07-21 04:56:57 -0700

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,13 @@
+class Datatype
+{
+public static void main(String args[])
+{
+int a=10;
+System.out.println("value of variable is= "+a+" size of int"+Interger.BYTES);
+char b='a';
+System.out.println("value of variable is= "+b+" size of char"+Character.BYTES);
+}
+}
+
+
+
