68728a024de8d10a5716ff7ecf853151950db08a
Anshu kumari <109532001+kumarianshu12@users.noreply.github.com>
2022-07-22 04:21:22 -0700

    Add files via upload

---
+++ AreaOfShape.java
@@ -0,0 +1,27 @@
+class AreaOfShape
+{
+double pi=3.14;
+public void area(int a)
+{
+
+System.out.println("area of sqaure= "+(a*a));
+}
+
+public void area(int a,int b)
+{
+System.out.println("area of rectangle= "+(a*b));
+
+}
+public void area(double r)
+{
+System.out.println("area of circle= "+(pi*r*r));
+}
+
+public static void main(String args[])
+{
+AreaOfShape obj =new AreaOfShape();
+obj.area(4);
+obj.area(5,3);
+obj.area(2,3);
+}
+}
