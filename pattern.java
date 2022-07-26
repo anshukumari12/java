62d96ecdffaf9b7e416c673dac0fb8e732a0a5f2
Anshu kumari <109532001+kumarianshu12@users.noreply.github.com>
2022-07-26 13:09:51 -0700

    pattern

---
+++ pattern.java
@@ -0,0 +1,26 @@
+
+class pattern
+{	
+public static void main(String[] args) 
+{		
+int i, j ,rows=5;		
+{		
+for(i=0;i<rows;i++) 
+{
+for(j=0;j<=i;j++) 
+{
+System.out.print("* ");
+}
+System.out.println();
+}		
+for(i=rows-1;i>=0;i--) 
+{
+for(j=0;j<=i-1;j++) 
+{
+System.out.print("* ");
+}
+System.out.println();
+}
+}
+}
+}
