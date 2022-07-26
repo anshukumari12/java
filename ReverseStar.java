eaf598708b53cf090ab98430126fbafd5be4e0fb
Anshu kumari <109532001+kumarianshu12@users.noreply.github.com>
2022-07-25 17:01:05 -0700

    ReverseStar

---
+++ ReverseStar.java
@@ -0,0 +1,17 @@
+class ReverseStar
+{
+public static void main(String args[])
+{
+int i,j;
+for(i=5;i>0;i--)
+{
+for(j=0;j<i;j++)
+{
+System.out.print("*");
+}
+{
+System.out.print("\n");
+}
+}
+}
+}
