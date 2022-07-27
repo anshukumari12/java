a463e39826cbad7783b76d85819d4a594ba906fe
Anshu kumari <109532001+kumarianshu12@users.noreply.github.com>
2022-07-27 16:58:05 -0700

    ScannerClass

---
+++ ScannerClass.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ScannerClass
+{
+public static void main(String args[])
+{
+Scanner scanner=new Scanner(System.in);
+System.out.println("enter any interger");
+int a=scanner.nextInt();
+System.out.println("input interger"+a);
+}
+}
