80ceb9e3afe194bd4d271230bbcede2bcd06bcf8
Anshu kumari <109532001+kumarianshu12@users.noreply.github.com>
2022-07-27 16:32:27 -0700

    Stringjava

---
+++ Stringjava.java
@@ -0,0 +1,10 @@
+class Stringjava
+{
+public static void main(String args[])
+{
+String javaString="Java";
+System.out.println("String" +javaString);
+System.out.println("In Lower" +javaString.toLowerCase());
+System.out.println("In Upper" +javaString.toUpperCase());
+}
+}
