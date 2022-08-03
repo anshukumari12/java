33ffb14a1026ce7c1500cbfdeac7506c7acbd4b6
Anshu kumari <109532001+kumarianshu12@users.noreply.github.com>
2022-08-02 20:31:33 -0700

    Sorting

--- Sorting.java
+++ Sorting.java
@@ -8,7 +8,7 @@ for(int i=0 ; i<array.length;i++)
 {
 for(int j=0;j<array.length;j++)
 {
-if(array[i]<array[j]);
+if(array[i]<array[j])
 {
 	 temp=array[i];
 	array[i]=array[j];
@@ -23,8 +23,3 @@ System.out.println(array[i]+ "  ");
 }
 }
 }
-
-
-
-
-
